e0a80d484a86ca899183b47cc061cb57f3b39979
Chardis <dxue@mozilla.com>
2016-11-24 10:30:47 +0800

    Add baidu geolocation

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoAppShell.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoAppShell.java
@@ -5,6 +5,12 @@
 
 package org.mozilla.gecko;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import java.util.Date;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -379,12 +385,66 @@ public class GeckoAppShell
         return lastKnownLocation;
     }
 
+    //Add for baidu geolocation
+    public static LocationClient mLocationClient = null;
+
+    private static void startBaiduLocation() {
+        if (mLocationClient != null) {
+            mLocationClient.requestLocation();
+            return;
+        }
+        final String BAIDU_PROVIDER = "BAIDU_GEOLOCATION";
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("gcj02");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=5000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死  
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+        // Register the listener
+        mLocationClient.registerLocationListener(new BDLocationListener() {
+          @Override
+          public void onReceiveLocation(BDLocation location) {
+            if (location == null) {
+              return;
+            }
+            // Baidu to Google location interfaces
+            Location mLocation = new Location(BAIDU_PROVIDER);
+            mLocation.setLatitude(location.getLatitude());
+            mLocation.setLongitude(location.getLongitude());
+            Date today = new Date();
+            mLocation.setTime(today.getTime());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
+                mLocation.setSpeed(location.getSpeed());
+            }
+            if (location.hasRadius()) {
+                mLocation.setAccuracy(location.getRadius());
+            }
+            getLocationListener().onLocationChanged(mLocation);
+          }
+        });
+        mLocationClient.start();
+    }
+
     @WrapForJNI(calledFrom = "gecko")
     @SuppressLint("MissingPermission") // Permissions are explicitly checked for within this method
     private static void enableLocation(final boolean enable) {
         final Runnable requestLocation = new Runnable() {
             @Override
             public void run() {
+                // Use baidu location client instead.
+                if (enable) {
+                    startBaiduLocation();
+                    return;
+                }
                 LocationManager lm = getLocationManager(getApplicationContext());
                 if (lm == null) {
                     return;
